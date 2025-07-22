12f99c911f7b43777d684402d252012a07f802df
khian calderon <ckhianvictory@gmail.com>
2025-07-22 14:54:03 +0800

    Added UI Shadcn + Create Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Create from "./section/create";
+
 export default function Home() {
   return (
-    <div className="p-4 md:p-8 w-full text-center">
-      <h1 className="text-4xl font-mono">Next.js + Supabase CRUD</h1>
-      <p className="mt-2 font-mono">by <a className="text-purple-600 underline" href="https://khian.netlify.app/" target="_blank">Khian Victory D. Calderon</a></p>
+    <div className="p-4 md:p-8 w-full">
+      <div className="text-center">
+        <h1 className="text-4xl font-mono">Next.js + Supabase CRUD</h1>
+        <p className="mt-2 font-mono">by <a className="text-purple-600 underline" href="https://khian.netlify.app/" target="_blank">Khian Victory D. Calderon</a></p>
+      </div>
+      <hr className="mt-6 mb-4"/>
+      <Create/>
     </div>
   )
 }
---
+++ app/section/create.tsx
@@ -0,0 +1,8 @@
+export default function Create () {
+    return (
+        <div className="font-thin">
+            <h1 className="text-2xl">Create</h1>
+            <span>This is create section</span>
+        </div>
+    )
+}
