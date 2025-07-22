13eea15e6961703446ec7902d34c49fa98e1431f
khian calderon <ckhianvictory@gmail.com>
2025-07-22 14:04:32 +0800

    Added Credits

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="p-4 md:p-8 w-full text-center">
       <h1 className="text-4xl font-mono">Next.js + Supabase CRUD</h1>
+      <p className="mt-2 font-mono">by <a className="text-purple-600 underline" href="https://khian.netlify.app/" target="_blank">Khian Victory D. Calderon</a></p>
     </div>
   )
 }
