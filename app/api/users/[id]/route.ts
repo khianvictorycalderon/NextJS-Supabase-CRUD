896794b9f747183141485ebbb1a5942a83d9cfb2
khian calderon <ckhianvictory@gmail.com>
2025-07-23 12:04:00 +0800

    Fix Attempt on Deployment 2

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
 export async function DELETE(
-    _: NextRequest,
+    _: Request,
     context: { params: { id: string }}
 ) {
     const { id } = context.params;
--- components/section/props.tsx
+++ components/section/props.tsx
@@ -1,4 +1,4 @@
-`use client`;
+'use client'
 
 export interface SubmitFeedbackProps {
   type: "default" | "success" | "warning" | "error";
