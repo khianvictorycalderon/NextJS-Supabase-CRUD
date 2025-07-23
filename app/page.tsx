6bec1b31c76daaf8d548c9a6d0a9ec6f19996d82
khian calderon <ckhianvictory@gmail.com>
2025-07-23 10:37:43 +0800

    Added Bottom Border

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import UpdateSection from "../components/section/update";
 export default function Home() {
 
   return (
-    <div className="p-4 md:p-8 w-full">
+    <div className="p-4 md:p-8 w-full mb-80">
       <div className="text-center">
         <h1 className="text-4xl font-mono">Next.js + Supabase CRUD</h1>
         <p className="mt-2 font-mono">by <a className="text-purple-600 underline" href="https://khian.netlify.app/" target="_blank">Khian Victory D. Calderon</a></p>
