1e467a22fd67b9676d9108ad470f0c5fecbca869
khian calderon <ckhianvictory@gmail.com>
2025-08-14 21:17:11 +0800

    Stopped Production

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import CreateSection from "../components/section/create";
-import DeleteSection from "../components/section/delete";
-import ReadSection from "../components/section/read";
-import UpdateSection from "../components/section/update";
+// import CreateSection from "../components/section/create";
+// import DeleteSection from "../components/section/delete";
+// import ReadSection from "../components/section/read";
+// import UpdateSection from "../components/section/update";
 
 export default function Home() {
 
@@ -11,11 +11,17 @@ export default function Home() {
         <h1 className="text-4xl font-mono">Next.js + Supabase CRUD</h1>
         <p className="mt-2 font-mono">by <a className="text-purple-600 underline" href="https://khian.netlify.app/" target="_blank">Khian Victory D. Calderon</a></p>
       </div>
-      <hr className="mt-6"/>
-      <CreateSection/>
+      <hr className="my-6"/>
+      <div className="text-center">
+        <h1 className="text-4xl font-mono">SORRY FOR THE INCONVENIENCE</h1>
+        <p className="mt-2 font-mono">
+          This project has been permanently stopped due to the limitations of the Supabase free-tier plan. This allows me to focus on my other projects. You can still access the source code <a className="text-purple-600 underline" href="https://github.com/khianvictorycalderon/NextJS-Supabase-CRUD" target="_blank">here</a>.
+        </p>
+      </div>
+      {/* <CreateSection/>
       <ReadSection/>
       <UpdateSection/>
-      <DeleteSection/>
+      <DeleteSection/> */}
     </div>
   )
 }
